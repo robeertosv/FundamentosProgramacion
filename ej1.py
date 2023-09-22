45642a01e1abdc906364431424e2423a26aaef40
robeertosv <=>
2023-09-22 12:43:33 +0200

    Ejericicio 1 de clase

---
+++ ej1.py
@@ -0,0 +1,7 @@
+myInt = 33
+myStr = "Roberto"
+myFloat = 3.45
+
+print(f"El número {myInt} es de tipo {type(myInt)} ")
+print(f"El string {myStr} es de tipo {type(myStr)} ")
+print(f"El número {myFloat} es de tipo {type(myFloat)} ")
