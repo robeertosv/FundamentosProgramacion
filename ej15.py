13bbfca54530aa6cb4fd7f5f53462119f5033437
robeertosv <=>
2023-09-23 20:59:03 +0200

    Ejercicio 15

---
+++ ej15.py
@@ -0,0 +1,12 @@
+"""
+15. Haz un programa que pida al usuario el nombre de las 2 frutas que tengo en
+mi frutero y cree una variable para el frutero y que imprima esta última.
+
+"""
+
+fruta_1 = input("Dime una fruta: ")
+fruta_2 = input("Dime otra fruta: ")
+
+frutero = fruta_1 + " y " + fruta_2
+
+print(f"En el frutero hay {frutero}")
