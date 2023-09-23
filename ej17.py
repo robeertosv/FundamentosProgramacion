5b4ff3c9a80e81b91ee027baef7d8d3eb7510470
robeertosv <=>
2023-09-23 21:13:56 +0200

    Ejercicio 17

---
+++ ej17.py
@@ -0,0 +1,12 @@
+"""
+17. Explica lo más brevemente posible qué hace la expresión: a=a==False,
+sabiendo que la variable a está inicializada a un valor bool (sea cual sea).
+Explica también qué pasaría si a no hubiera sido previamente inicializado.
+
+"""
+
+"""
+    La primera expresión guarda en la variable a el resultado de comparar a con False, si a es False se guardará True, si no, se guardará False
+    
+    Si no se hubiese inicializado la variable a como Bool python diría que a no está definida
+"""
