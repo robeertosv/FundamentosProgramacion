92eed2dc56e81e1f2fc969d88c05d061eeff08b8
robeertosv <=>
2023-09-29 12:20:19 +0200

    Ejercicio 8

--- Cuaderno_2/ej8.py
+++ Cuaderno_2/ej8.py
@@ -6,3 +6,26 @@ Prueba tu función.
     
 """
 
+a = (1, 2, 4)
+b = (4, 5, 6)
+
+
+def calc_vector(a, b) -> list:
+    v = [0, 0, 0]  # v es el vector que va de a a b (AB)
+
+    for i in range(len(a)):
+        v[i] = b[i] - a[i]
+    
+    return v
+    
+def calc_vector_module(vec:list) -> int:
+    import math
+    
+    m = 0
+    
+    for i in range(len(vec)):
+        m += vec[i] ** 2
+    
+    return math.sqrt(m)
+
+print(calc_vector_module(calc_vector(a,b)))
