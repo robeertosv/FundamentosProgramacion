5bb2984aba0b11213a067a6756ab0b603c72c2b6
robeertosv <=>
2023-09-29 10:26:18 +0200

    Ej8 y abs

---
+++ Cuaderno_2/ej8.py
@@ -0,0 +1,8 @@
+"""
+Escribe una función que a partir de las coordenadas 3D de dos puntos en el
+espacio en formato (x, y, z) calcule la distancia que hay entre dichos puntos.
+
+Prueba tu función. 
+    
+"""
+
