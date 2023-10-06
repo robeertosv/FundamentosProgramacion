06f56dcfc9d21fb8a38c60bcdcbec69274f399fe
robeertosv <=>
2023-10-06 13:02:40 +0200

    Ejercicio 3

---
+++ Cuaderno_3A/ej3.py
@@ -0,0 +1,22 @@
+"""
+Como habrás observado, muy a menudo necesitamos validar la información
+introducida por el usuario. Programa una función “validar_entero” que se asegure
+de que una entrada del usuario es un entero. Ten en cuenta que dicha entrada
+puede ser cualquier cosa, por ejemplo, un valor real, un booleano o incluso un texto
+como “Hola”. Nuestra función recibirá un texto y retornará verdadero si es un
+entero (dando por válida la entrada) o falso (rechazando la entrada).
+
+"""
+
+en = input("Introduce un valor: ")
+
+def validar_entero(inp)->bool:
+    try:
+        int(inp)
+    except:
+        return False
+    else: 
+        return True
+        
+
+print(validar_entero(en))
