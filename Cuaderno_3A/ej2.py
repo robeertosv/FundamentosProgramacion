ef11d7c991a06bb724e5560ad2a8e9619656b64c
robeertosv <=>
2023-10-06 12:56:42 +0200

    Ejercicio 2

--- Cuaderno_3A/ej2.py
+++ Cuaderno_3A/ej2.py
@@ -7,14 +7,17 @@ asegurarte de que se trata de valores en el rango correcto.
 """
 import math
 
-
-def converter(horas: int, minutos: int) -> str:
-
-    if (horas > 24 or horas < 0 or minutos > 59 or minutos < 0):
-        if (horas < 12):
-            return str(horas) + ":" + str(minutos) + "AM"
-        else:
-            return str(horas-12) + ":" + str(minutos) + "PM"
-
+def converter(horas:int, minutos:int)->str:
+    if(type(horas) != 'int' or type(minutos) != 'int'):
+        if(horas < 24 or horas > 0 or minutos < 59 or minutos > 0):
+            if(horas < 12):
+                return str(horas) + ":" + str(minutos) + "AM"
+            else:
+                return str(horas-12) + ":" + str(minutos) + "PM"
+    else:
+        return "ERROR"
+    
+    
 
 print(converter(13, 34))
+print(converter(11, 40))
