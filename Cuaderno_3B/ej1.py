c006ee8255afcf9a79ad9f9d25183f8a9c8cdbd3
robeertosv <=>
2023-10-13 12:13:07 +0200

    Update files

---
+++ Cuaderno_3B/ej1.py
@@ -0,0 +1,16 @@
+"""
+Ya vimos que la validación de entradas es algo tan presente que no podemos
+escapar de ello, así que si te parece vamos a escribir una función que solicite al
+usuario introducir un entero y que no pare de pedírselo hasta que la información
+introducida sea válida.
+
+La idea es usar la construcción while combinada con la función de validación
+programada en cuadernos anteriores, consiguiendo una función cuya cabecera
+sería la siguiente:
+
+def leer_entero_validado():
+ "" None --> int
+ OBJ: Solicita un entero al usuario, lo valida y lo retorna sólo
+ cuando se ha asegurado de que es realmente un entero
+ ""
+"""
---
+++ Ejemplos_Clase/triangulo.py
@@ -0,0 +1,6 @@
+a = []
+for i in range(1,6):
+    for j in range(i):
+        a.append('*')
+
+print(a)
