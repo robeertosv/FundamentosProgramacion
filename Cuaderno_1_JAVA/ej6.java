e3d8e58bc05084d45d7b11d477f1824e077a0e90
robeertosv <=>
2023-10-14 23:12:54 +0200

    EJ_6

--- Cuaderno_1_JAVA/ej6.java
+++ Cuaderno_1_JAVA/ej6.java
@@ -1,17 +1,3 @@
-/*
- distancia = float(input("Cuantos km has hechado en el asfalto?\n"))
-litrosFuel = float(input("Cuanto diesel has hechado?\n"))
-precioFuel = float(input("A cuanto está el diesel?\n"))
-costesVarios = float(input("En otros gastos, ¿cuánto te has fundido?\n"))
-
-costeGasolina = litrosFuel * precioFuel
-kilometroPorLitro = distancia / litrosFuel
-costeTotalPorKM = (costeGasolina + costesVarios) / distancia
-
-print(f"En gasolina has gastado {costeGasolina}€ \nEl consumo ha sido de {kilometroPorLitro} km/L \nEl coste total \
-por kilómetro ha sido de {costeTotalPorKM} €/km")
-
- */
 import java.util.Scanner;
 
 public class ej6 {
