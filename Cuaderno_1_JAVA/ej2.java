d093586767592d8372cf8c52dd004d31126976b7
robeertosv <=>
2023-10-14 22:54:47 +0200

    EJ_2

---
+++ Cuaderno_1_JAVA/ej2.java
@@ -0,0 +1,7 @@
+//2. Escribe un código que imprima tu nombre y edad, así: "Nombre: Pedro Martín, edad: 25"
+
+public class ej2 {
+    public static void main(String []args) {
+        System.out.println("Nombre: Roberto Seco, edad: 25");
+    }
+}
