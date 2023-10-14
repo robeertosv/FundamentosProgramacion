65676bcea840cc3337f2720b77d7de844bf53646
robeertosv <=>
2023-10-14 22:56:45 +0200

    EJ_3

---
+++ Cuaderno_1_JAVA/ej3.java
@@ -0,0 +1,6 @@
+//Calcular y mostrar por pantalla el área de un cuadrado
+public class ej3 {
+    public static void main(String []args) {
+        System.out.println("El área del cuadrado de lado 2 es: " + 2*2);
+    }
+}
