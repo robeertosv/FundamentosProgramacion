c5ee5d9a7282290008ffc5ca1624f6c519b18b9c
robeertosv <=>
2023-10-15 13:01:20 +0200

    EJ_11

---
+++ Cuaderno_1_JAVA/ej11.java
@@ -0,0 +1,14 @@
+public class ej11 {
+    public static void main(String[] args) {
+        float a = 3 / 2;
+        float b = 3.0f / 2;
+
+        System.out.println("a= " + a + " b = " + b);
+    }
+}
+/*
+ * La variable "a" es una división entre dos números enteros que da como
+ * resultado un decimal
+ * La variable "b" es una divión entre un float y un int que como resultado da
+ * un float
+ */
