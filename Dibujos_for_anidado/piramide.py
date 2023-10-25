2af10e9d9dd8713b95039a86219c991eac82ba6f
robeertosv <=>
2023-10-25 20:06:30 +0200

    Piramide

--- Dibujos_for_anidado/piramide.py
+++ Dibujos_for_anidado/piramide.py
@@ -1,6 +1,6 @@
 def piramide(n):
-    for i in range(n):
-        for j in range(i, n*2-1):
+    for i in range(1, n):
+        for j in range(i, n):
             print(' ', end="")
             
         for k in range(i):
