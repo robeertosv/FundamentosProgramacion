97851ed35b1ce80affc050e10e0a4adfa267ebe3
robeertosv <=>
2023-10-25 20:08:37 +0200

    Comentados

--- Dibujos_for_anidado/blade.py
+++ Dibujos_for_anidado/blade.py
@@ -1,3 +1,4 @@
+"""Piensa este como una matriz, quiero poner una * en la diagonal (cnd i == j) o en la diagonal contraria (cnd i == n+1-j)"""
 def blade(n:int):
     for i in range(1, n+1):
         for j in range(1, n+1):
--- Dibujos_for_anidado/cruz.py
+++ Dibujos_for_anidado/cruz.py
@@ -1,4 +1,6 @@
 def cruz(n:int):
+    
+    """La idea es pintar los palos de arriba de la cruz (la mitad por eso //2), luego la fila llena de cruces, y luego la otra mitad"""
     for i in range(n//2):
         for j in range(n//2):
             print('   ', end="")
