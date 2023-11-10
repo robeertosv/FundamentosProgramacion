428c798e050b32e8e48615e60c0e86a632878dda
robeertosv <=>
2023-11-10 13:13:08 +0100

    Refactor code for improved readability and performance.

--- Cuaderno_4/ej6.py
+++ Cuaderno_4/ej6.py
@@ -3,7 +3,7 @@ Implementar una función que compruebe si una palabra es un palíndromo.
 Atención, no hagas más trabajo del necesario. 
 """
 
-def is_palindrome(palabra):
+def is_palindrome(palabra:str)->bool:
     palindrome = None
     inversa = palabra[::-1]
     
