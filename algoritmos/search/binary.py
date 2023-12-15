ffd1b8f448b7e88ae13e841a4954a1106c134166
robeertosv <=>
2023-12-15 11:16:36 +0100

    Refactor code to improve performance and readability

--- algoritmos/search/binary.py
+++ algoritmos/search/binary.py
@@ -1,9 +1,10 @@
-arr = [2,8,7,3,51,58,63,35,68,1,3,8,3,8,14,54,13,384,185,31,138,16]
+#arr = [2,8,7,3,51,58,63,35,68,1,3,8,3,8,14,54,13,384,185,31,138,16]
+arr = ['roberto', 'luis', 'paco']
 
 # Se trata de ir partiendo el array en mitades, para ello tiene que estar ordenado
 def binarySearch(arr:list, elem:int)->str:
     arr.sort() # El array tiene que estar ordenado, se puden usar los algoritmos quickSort, insertionSort, bubble... Pero este ya te lo da Python
-    
+    print(arr)
     found = False
     i = 0
     
@@ -25,4 +26,4 @@ def binarySearch(arr:list, elem:int)->str:
     
     return f'{elem} encontrado en la posición {index}' if found == True else f'El número {elem} no está en la lista'
 
-print(binarySearch(arr, 5))
+print(binarySearch(arr, 'paco'))
---
+++ seyam.py
@@ -0,0 +1,4 @@
+def f():
+    print('hola')
+    import seyam
+f()
