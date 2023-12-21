42905332b0f9f54ae9b526f7d9931df32bd32b6a
robeertosv <=>
2023-12-21 17:00:49 +0100

    colegios

--- PL2/colegios/colegios.py
+++ PL2/colegios/colegios.py
@@ -81,3 +81,65 @@ def ordenar_por_ciudad(colegios):
     return colegios
 
 #print(ordenar_por_ciudad(colegios))
+
+"""
+A partir de un texto (por ejemplo una reseña en un periódico), crear un diccionario que contenga
+el número de veces que aparece la ciudad de los colegios de la competición en el texto y mostrar esas
+frecuencias en un histograma.
+Para el texto:
+"""
+texto='Los colegios de Madrid demostraron un nivel muy alto, aunque los de Toledo. Soria se quedó un poco rezagada y Burgos demostró un gran nivel también. Tendremos que esperar para ver quién gana la final, pero el colegio Sto. Domingo de Madrid es un firme candidato. Por su parte, Merindades, de Burgos y Virgen de Covadonga, de Madrid, están muy empatados.'
+
+def extractor_ciudades(palabra, colegios):
+    ciudad = ''
+    
+    ciudades = []
+    
+    for colegio in colegios:
+        if colegio['ciudad'] not in ciudades:
+            ciudades.append(colegio['ciudad'])
+    
+    if palabra in ciudades:
+        ciudad = palabra
+    elif palabra[1:] in ciudades:
+        ciudad = palabra[1:]
+    elif palabra[0:len(palabra)-1] in ciudades:
+        ciudad = palabra[0: len(palabra)-1]
+    elif palabra[1:len(palabra)-1]:
+        ciudad = palabra[1:len(palabra)-1]
+    
+    return ciudad
+def generar_histograma(texto, colegios):
+    ciudades = []
+    c = {}
+    for colegio in colegios:
+        if colegio['ciudad'] not in ciudades:
+            ciudades.append(colegio['ciudad'])
+    
+    for ciudad in ciudades:
+        c.setdefault(ciudad, 0)
+        
+    texto = texto.split(' ')
+    for word in texto:
+        if extractor_ciudades(word, colegios) in ciudades:
+            ciu = extractor_ciudades(word, colegios)
+            c[ciu] += 1
+    
+    return c
+
+hist = generar_histograma(texto, colegios)
+    
+def mostrar_histograma(hist):
+    
+    ciudades = []
+    
+    for ciudad in hist:
+        ciudades.append(ciudad)
+    
+    for ciudad in ciudades:
+        print(f'{ciudad}: ', end="")
+        for i in range(hist[ciudad]):
+            print('#', end="")
+        print()
+
+mostrar_histograma(hist)
